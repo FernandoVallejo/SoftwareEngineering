f68341cffe187154d501c86ea15061e9cfd9e52d
Volfra <ing.wsoto@gmail.com>
2023-03-31 16:35:30 -0500

    ObjectPool + Reflection

--- designPatterns/src/co/edu/poli/objectPool/model/Client.java
+++ designPatterns/src/co/edu/poli/objectPool/model/Client.java
@@ -8,23 +8,23 @@ public class Client {
 		Giant g1 = (Giant) pool.checkOut("Giant");
 		System.out.println("created "+ g1.getId() +" "+ g1);
 		System.out.println(pool);
-		Dino g2 = (Dino) pool.checkOut("Dino");
-		System.out.println("created "+ g2.getId() +" "+ g2);
+		Dino d1 = (Dino) pool.checkOut("Dino");
+		System.out.println("created "+ d1.getId() +" "+ d1);
 		System.out.println(pool);
-		Giant g3 = (Giant) pool.checkOut("Giant");
-		System.out.println("created "+ g3.getId() +" "+ g3);
+		Giant g2 = (Giant) pool.checkOut("Giant");
+		System.out.println("created "+ g2.getId() +" "+ g2);
 		System.out.println(pool);
 		pool.checkIn(g1);
 		System.out.println("Insert at pool "+ g1.getId() +" "+ g1);
 		System.out.println(pool);
-		Giant g4 = (Giant) pool.checkOut("Giant");
-		System.out.println("created "+ g4.getId() +" "+ g4);
+		Giant g3 = (Giant) pool.checkOut("Giant");
+		System.out.println("created "+ g3.getId() +" "+ g3);
 		System.out.println(pool);
-		pool.checkIn(g2);
-		System.out.println("Insert at pool "+ g2.getId() +" "+ g2);
+		pool.checkIn(d1);
+		System.out.println("Insert at pool "+ d1.getId() +" "+ d1);
 		System.out.println(pool);
-		Dino g5 = (Dino) pool.checkOut("Dino");
-		System.out.println("Extract object of the pool "+ g5.getId() +" "+ g5);
+		Dino d2 = (Dino) pool.checkOut("Dino");
+		System.out.println("Extract object of the pool "+ d2.getId() +" "+ d2);
 		System.out.println(pool);
 		
 	}
